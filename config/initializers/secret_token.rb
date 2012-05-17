cd41ef89bb7d9a04eb946e6518a3088a79554efe
Michael Alexander <beefsack@gmail.com>
2012-05-17 21:41:05 +0800

    Initial project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EzCommerce::Application.config.secret_token = '925851ca67587ab4451a49c621d79ab96fb7243d69931a149a089b60bd3cf7f3524d776df74b5351117641087a5bda5368647f85c597de7a73c09425e73b4f0a'
